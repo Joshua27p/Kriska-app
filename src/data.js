3e77c5f0af8ff771372f84dbf894473e5533841f
Yoshua_Pequeño <josh27p=git config --global user.email josh27p@gmail.com>
2020-12-26 10:15:57 -0500

    share code on github

---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating:'4.5',
+            numreviews:10,
+            description:'high quality Product'
+        },
+        {
+            _id:'2',
+            name:'Nike Slim Shirt',
+            category: 'shirts',
+            image: '/images/p2.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating:'4.5',
+            numreviews:10,
+            description:'high quality Product'
+        },
+        {
+            _id:'3',
+            name:'Nike Slim Shirt',
+            category: 'shirts',
+            image: '/images/p3.jpg',
+            price: 110,
+            brand: 'Nike',
+            rating:'4.5',
+            numreviews:10,
+            description:'high quality Product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Shirt',
+            category: 'shirts',
+            image: '/images/p4.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating:'4.5',
+            numreviews:10,
+            description:'high quality Product'
+        },
+        {
+            _id:'5',
+            name:'puma shirt',
+            category: 'shirts',
+            image: '/images/p5.jpg',
+            price: 40,
+            brand: 'Puma',
+            rating:'5',
+            numreviews:10,
+            description:'high quality Product'
+        },
+        {
+            _id:'6',
+            name:'Lacoste pants',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 30,
+            brand: 'Lacoste',
+            rating:'5',
+            numreviews:5,
+            description:'high quality Product'
+        }
+
+    ]
+}
+
+export default data;
