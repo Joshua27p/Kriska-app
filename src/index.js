a93d51d942cde51f721c94687fe62f232e1ca520
Paul Tang <paul.tang95@gmail.com>
2020-12-28 15:27:58 -0500

    backend y redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM.render(<App />,document.getElementById('root')
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+    ,document.getElementById('root')
 );
 
  
